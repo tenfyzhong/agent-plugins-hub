84ec2de34ed25d1da471782ee9365c8780f76e39
tenfyzhong <tenfy@tenfy.cn>
2026-08-22 23:06:57 +0800

    chore: remove agent-usage-exporter plugin
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- plugins/agent-usage-exporter/extensions/agent-usage-exporter-omp.ts
+++
@@ -1,5 +0,0 @@
-import { registerAgentUsageExporter } from "./agent-usage-exporter.ts";
-
-export default function agentUsageExporterOmp(pi: Parameters<typeof registerAgentUsageExporter>[0]) {
-  registerAgentUsageExporter(pi, "oh-my-pi", "session_stop");
-}
